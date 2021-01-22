3b521b573cfbb9df278a790307511fc0a0ae4576
Radu Popescu <mradupopescu@yahoo.com>
2021-01-23 00:52:03 +0200

    complete tests

--- sales-management/src/main/java/com/salesmanagement/salesmanagement/repository/ProductRepository.java
+++ sales-management/src/main/java/com/salesmanagement/salesmanagement/repository/ProductRepository.java
@@ -4,7 +4,11 @@ import com.salesmanagement.salesmanagement.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+
+    public Product findByProductName(String productName); // added for ProductTests class
 
 }
--- sales-management/src/test/java/com/salesmanagement/salesmanagement/ProductTests.java
+++ sales-management/src/test/java/com/salesmanagement/salesmanagement/ProductTests.java
@@ -7,6 +7,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class ProductTests {
@@ -17,6 +23,56 @@ public class ProductTests {
     @Test
     public void testCreateProduct(){
         Product product = new Product("Sony","TV");
-        repo.save(product);
+        Product savedProduct = repo.save(product);
+
+        assertNotNull(savedProduct);
     }
+
+    @Test
+    public void testFindProductByName(){
+        String name = "Adidas";
+        Product product = repo.findByProductName(name);
+
+        assertThat(product.getProductName()).isEqualTo(name);
+    }
+
+    @Test
+    public void testDoesNotFindProductByName(){
+        String name = "Reebok";
+        Product product = repo.findByProductName(name);
+
+        assertNull(product);
+    }
+
+
+    @Test
+    public void testUpdateProduct(){
+        String productName = "Adidas";
+        Product product = new Product("ssss", "shoes");
+        product.setId(3L);
+
+        repo.save(product);
+        Product updatedProduct = repo.findByProductName(productName);
+
+        assertThat(updatedProduct.getProductName()).isEqualTo(productName);
+    }
+
+    @Test
+    public void testListProducts(){
+        List<Product> products = (List<Product>) repo.findAll();
+
+        assertThat(products).size().isGreaterThan(0);
+    }
+
+    @Test
+    public void testDeleteProduct(){
+        Long id = 2L;
+        boolean existsBeforeDelete = repo.findById(id).isPresent();
+        repo.deleteById(id);
+        boolean doesNotExistAfterDelete = repo.findById(id).isPresent();
+
+        assertTrue(existsBeforeDelete);
+        assertFalse(doesNotExistAfterDelete);
+    }
+
 }
